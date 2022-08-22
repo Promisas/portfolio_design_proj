f8450a4d47f7c7a8c367e2fa7bd1924ddd85ca0e
Promisas <Promiseup4@gmail.com>
2022-08-22 19:52:50 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700">
+    <link rel="stylesheet" href="text.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+    <title>Portfolio nr.1</title>
+</head>
+
+<body>
+    <div id="particles-js"></div>
+    <header>
+        <ul>
+            <li>
+                <a class="wow fadeInDown" data-wow-duration=".5s" data-wow-delay="1s" href="#home">Home</a>
+                <a class="wow fadeInDown" data-wow-duration=".5s" data-wow-delay="1.1s" href="#about">About</a>
+                <a class="wow fadeInDown" data-wow-duration=".5s" data-wow-delay="1.2s" href="#service">Service</a>
+                <a class="wow fadeInDown" data-wow-duration=".5s" data-wow-delay="1.3s" href="#gallery">Gallery</a>
+                <a class="wow fadeInDown" data-wow-duration=".5s" data-wow-delay="1.4s" href="#contact">Contact</a>
+            </li>
+        </ul>
+    </header>
+
+    <!-- home section-->
+
+    <section id="home" class="home wow fadeIn" data-wow-duration="2s" data-wow-delay="1.2s">
+        <div class="name">
+            <h1 class="wow fadeInUp" data-wow-duration="3s" data-wow-delay="1.9s">Name Surname</h1>
+            <h5 class="wow fadeInDown" data-wow-duration="4s" data-wow-delay="1.4s">Short description</h5>
+        </div>
+    </section>
+
+    <!--about section-->
+    <section id="about" class="about">
+        <div class="section-title">
+            <h2 class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">About</h2>
+        </div>
+        <div class="about-info">
+            <p class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay=".9s">Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Beatae commodi consequatur corporis culpa ducimus
+                ex harum hic, impedit itaque iure labore laboriosam maxime modi mollitia nulla odit optio sint
+                voluptatibus.</p>
+            <p class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1s">Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Animi dolor dolore, dolores eum laboriosam mollitia
+                odit omnis optio quos rem. Animi distinctio explicabo fugit impedit ipsam, iste necessitatibus officia
+                reprehenderit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium dicta dolores iusto
+                omnis rerum sunt voluptatibus voluptatum. Ea esse exercitationem expedita, iure, minima, nulla placeat quis
+                reiciendis repudiandae soluta tempora.</p>
+            <a href="#" class="downloadbtn wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.2s">Download Resume</a>
+
+
+            <div class="timeline">
+                <div class="col wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">
+                    <span> 2015 - 2021</span>
+                    <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio exercitationem natus.</h6>
+                </div>
+                <div class="col wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.3s">
+                    <span> 2015 - 2021</span>
+                    <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio exercitationem natus.</h6>
+                </div>
+                <div class="col wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.6s">
+                    <span> 2015 - 2021</span>
+                    <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio exercitationem natus.</h6>
+                </div>
+                <div class="col wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.8s">
+                    <span> 2015 - 2021</span>
+                    <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio exercitationem natus.</h6>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--service section-->
+    <section id="service" class="service">
+        <div class="section-title">
+            <h2 class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">Service</h2>
+        </div>
+        <div class="service-info">
+            <div class="col">
+                <img class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay=".9s" src="https://picsum.photos/g/250/150/?random" alt="">
+                <h3 class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">text</h3>
+                <p class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.2s">Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit. Adipisci asperiores aspernatur consectetur
+                    consequatur delectus deserunt dolore eveniet inventore itaque nesciunt nihil, of</p>
+            </div>
+            <div class="col">
+                <img class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay=".9s" src="https://picsum.photos/g/250/150/?random" alt="">
+                <h3 class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">text</h3>
+                <p class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.2s">Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit. Adipisci asperiores aspernatur consectetur
+                    consequatur delectus deserunt dolore eveniet inventore itaque nesciunt nihil, of</p>
+            </div>
+            <div class="col">
+                <img class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay=".9s" src="https://picsum.photos/g/250/150/?random" alt="">
+                <h3 class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">text</h3>
+                <p class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.2s">Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit. Adipisci asperiores aspernatur consectetur
+                    consequatur delectus deserunt dolore eveniet inventore itaque nesciunt nihil, of</p>
+            </div>
+            <div class="col">
+                <img class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay=".9s" src="https://picsum.photos/g/250/150/?random" alt="">
+                <h3 class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">text</h3>
+                <p class="wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.2s">Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit. Adipisci asperiores aspernatur consectetur
+                    consequatur delectus deserunt dolore eveniet inventore itaque nesciunt nihil, of</p>
+            </div>
+        </div>
+    </section>
+
+    <!--gallery section-->
+    <div id="gallery" class="gallery">
+        <div class="section-title">
+            <h2 class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">Gallery</h2>
+        </div>
+        <div class="gallery-info">
+            <div class="col wow fadeInUp" data-wow-duration="1.3s" data-wow-delay=".9s">
+                <img src="https://picsum.photos/g/250/150/?random" alt="">
+            </div>
+            <div class="col wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1s">
+                <img src="https://picsum.photos/g/250/150/?random" alt="">
+            </div>
+            <div class="col wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">
+                <img src="https://picsum.photos/g/250/150/?random" alt="">
+            </div>
+            <div class="col wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.2s">
+                <img src="https://picsum.photos/g/250/150/?random" alt="">
+            </div>
+            <div class="col wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.3s">
+                <img src="https://picsum.photos/g/250/150/?random" alt="">
+            </div>
+            <div class="col wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.4s">
+                <img src="https://picsum.photos/g/250/150/?random" alt="">
+            </div>
+            <div class="col wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.5s">
+                <img src="https://picsum.photos/g/250/150/?random" alt="">
+            </div>
+            <div class="col wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.6s">
+                <img src="https://picsum.photos/g/250/150/?random" alt="">
+            </div>
+            <div class="col wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.7s">
+                <img src="https://picsum.photos/g/250/150/?random" alt="">
+            </div>
+        </div>
+    </div>
+
+    <!--contact section-->
+    <section id="contact" class="contact">
+        <div class="section-title">
+            <h2 class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">Contact</h2>
+        </div>
+        <div class="contact-info">
+            <form action="">
+                <input type="text" class="textField wow fadeInUp" data-wow-duration="1.3s" data-wow-delay=".9s" placeholder="Full Name" required="">
+                <input type="text" class="textField wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1s" placeholder="Email Address" required="">
+                <textarea class="textField messageField wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s" placeholder="Your Message"></textarea>
+                <input type="submit" class="textField submitButton wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.8s"" value=" Send Message">
+            </form>
+        </div>
+    </section>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js"></script>
+    <script src="particle-script.js"></script>
+    <script type="text/javascript">
+        new WOW().init();
+
+        $(window).scroll(function() {
+            var sticky = $("header");
+            if ($(window).scrollTop() >= 100) {
+                sticky.addClass("fixed");
+            } else {
+                sticky.removeClass("fixed");
+            }
+        });
+    </script>
+</body>
+
+</html>
